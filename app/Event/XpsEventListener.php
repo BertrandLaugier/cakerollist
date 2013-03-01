f5d8c0a7a5ee126bd116b5237617c2a7d0bc58aa
goomies <remy_manescau3@hotmail.com>
2013-03-01 18:09:42 +0100

    Xp add when message send

--- app/Event/XpsEventListener.php
+++ app/Event/XpsEventListener.php
@@ -1,27 +1,30 @@
 <?php 
-	App::uses('CakeEventListener','Event');
+App::uses('CakeEventListener','Event');
 
-	/**
-	* 
-	*/
+
 	class XpsEventListener implements CakeEventListener
 	{
 		
 		public function implementedEvents()
 		{
 			return array(
-				'Model.Message.add' => 'userAddMessage');
+				'Model.Message.add' => 'userSendMessage');
 		}
 
-		public function userAddMessage($event){
+		public function userSendMessage($event){
 			$user_id = $event->subject()->data['Message']['user_id'];
 			$message = ClassRegistry:: init('Message');
 			$count = $message->find('first', array(
 				'conditions' => array('User.id'=> $user_id)
 			));
 			
-			$xp = $count['User']['xp_nb'];
-			$this->addXP($user_id,150);
+			// Recupère la quantité d'xp de l'utilisateur
+			$xp_nb = $count['User']['xp_nb'];
+			// Recupère le niveau de l'utilisateur
+			$xp_id = $count['User']['xp_id'];
+
+
+			$this->addXP($xp_id,$user_id,$xp_nb,$count,10);
 		}
 		
 		/**
@@ -31,38 +34,54 @@
 		 * @param int $user_id 
 		 * @param int $xp XP to add
 		 * @return void
-		 * @author gaspard
+		 * @author Remy
 		 */
-		public function addXP($user_id,$xp){
+		public function addXP($xp_id,$user_id,$xp_nb,$count,$xp){
 			
+			// Ajoute 10 d'xp à l'envoie d'un message
+			$new_xp= $xp_nb + $xp;
+
 			
-			debug($xp);
-			//$this->checkLevel($user_id);
+
+		
+			$this->checkLevel($xp_id,$new_xp);
+		
 		}
-		
-		
 		/**
 		 * check level
 		 * check if the level is suited to the xp
 		 * 
 		 * @param int $user_id 
 		 * @return to determine
-		 * @author gaspard
+		 * @author Remy 
 		 */
-		public function checkLevel($user_id){
-			$levels = array(100,150,225,350,500,100000);
-			/*
+		public function checkLevel($xp_id,$new_xp){
 			
-				if($xp > 100 && level < 2){
-					je passe à level 2
+			// Déclaration des paliers pour chaque niveau
+			$levels = array(0,100,150,220,350,500,1000);
+
+   				// Vérifie si l'xp de l'utilisateur correspond au palier pour gagner un niveau
+				if($new_xp == $levels[$xp_id]){
+					$xp_id = $xp_id+1;
+
+					// Reset l'xp quand on gagne un niveau
+					$new_xp = 0;	
+					
+					//Sauvegarde l'xp et le niveau de l'utilisateur
+					//if ($this->request->is('post')) {
+					//	$this->Message->create();
+					//	$this->request->data['Message']['user_id'] = $this->Auth->user('id');
+					//	if ($this->Message->save($this->request->data)) {
+					//		$this->Session->setFlash(__('The message has been saved'));
+					//		$this->redirect(array('action' => 'index'));
+					//}
+					debug($xp_id);
+					die();					
 				}
-				if($xp > 400 && level < 3){
-					je passe à level 3
+				// Sauvegarder l'xp si l'utilisateur ne gagne pas de niveau
+				else{
+
 				}
-			*/
-			
-			
-		}
-		
+		}		
 	}
  ?>
